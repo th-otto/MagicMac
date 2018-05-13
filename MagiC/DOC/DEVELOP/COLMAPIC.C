c6fdcdb3a7808b70ea97dfe534ff530709842aa6
Andreas Kromke <andreas.kromke@web.de>
2018-05-13 14:22:09 +0200

    older documentation and missing tools

---
+++ MagiC/DOC/DEVELOP/COLMAPIC.C
@@ -0,0 +1,16 @@
+/*
+* übergib rgb-Tripel in Promille für die Iconwandlung
+* bei "direct colour" Bildschirm-Modi.
+*
+* Ab MagiC 5.20 vom 20.11.97
+*/
+
+void *sys_recalc_cicon_colours( UWORD colour_values[3*256] )
+{
+	PARMDATA d;
+	static WORD	c[] = { 0, 1, 1, 0 };
+
+	d.intin[0] = 5;
+	d.addrin[0] = colour_values;
+	_mt_aes( &d, c, NULL );
+}
