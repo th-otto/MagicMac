1d492d6eddc24d9feab7b1577f1cef54636b5e71
Thorsten Otto <admin@tho-otto.de>
2018-10-18 14:18:02 +0200

    Reconstruct source of filediv.c

---
+++ kernel/vdi/filediv.c
@@ -0,0 +1,45 @@
+#include <tos.h>
+#include <stddef.h>
+#include "filediv.h"
+
+
+char *load_file(const char *filename, long *size)
+{
+	DTA *olddta;
+	DTA dta;
+	char *buf = NULL;
+	
+	olddta = Fgetdta();
+	Fsetdta(&dta);
+	if (Fsfirst(filename, FA_CHANGED|FA_RDONLY|FA_HIDDEN|FA_SYSTEM) == 0)
+	{
+		buf = Malloc_sys(dta.d_length);
+		if (buf != NULL)
+		{
+			*size = read_file(filename, buf, 0, dta.d_length);
+			if (*size != dta.d_length)
+			{
+				Mfree_sys(buf);
+				buf = NULL;
+			}
+		}
+	}
+	Fsetdta(olddta);
+	return buf;
+}
+
+
+long read_file(const char *filename, char *buf, long offset, long size)
+{
+	long fd;
+	long retsize = 0;
+	
+	fd = Fopen(filename, FO_READ);
+	if (fd > 0)
+	{
+		Fseek(offset, (short)fd, SEEK_SET);
+		retsize = Fread((short)fd, size, buf);
+		Fclose((short)fd);
+	}
+	return retsize;
+}
