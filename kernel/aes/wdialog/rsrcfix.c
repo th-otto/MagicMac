e351fc7b3e9fae81158abe0aede34b16a3ecc4f8
Thorsten Otto <admin@tho-otto.de>
2018-11-21 13:11:54 +0100

    Optimize get_address() by using tables

--- kernel/aes/wdialog/rsrcfix.c
+++ kernel/aes/wdialog/rsrcfix.c
@@ -1,246 +1,122 @@
 #include "wdlgmain.h"
-
-#define strlen mystrlen
-
-
-static WORD *gl_global;
-static RSHDR *gl_rsc;
-
-static void fix_treeindex(void);
-static void fix_tedinfo(void);
-static int fix_long(LONG *lptr);
-static void *get_address(WORD type, WORD idx);
-static int fix_ptr(WORD type, WORD idx);
-static void fix_nptr(WORD idx, WORD ob_type);
-static void fix_object(void);
-static void fix_obj(OBJECT *tree, WORD obj);
-static void fix_chp(WORD *pcoord, int hor);
+#include <stddef.h>
 
 
-void _rsrc_rcfix(WORD *global, RSHDR *rsc)
+static unsigned char const rg_type_tab[] = {
+	offsetof(RSHDR, rsh_trindex) / sizeof(UWORD),  /* R_TREE */
+	offsetof(RSHDR, rsh_object) / sizeof(UWORD),   /* R_OBJECT */
+	offsetof(RSHDR, rsh_tedinfo) / sizeof(UWORD),  /* R_TEDINFO */
+	offsetof(RSHDR, rsh_iconblk) / sizeof(UWORD),  /* R_ICONBLK */
+	offsetof(RSHDR, rsh_bitblk) / sizeof(UWORD),   /* R_BITBLK */
+	offsetof(RSHDR, rsh_frstr) / sizeof(UWORD),    /* R_STRING */
+	offsetof(RSHDR, rsh_frimg) / sizeof(UWORD),    /* R_IMAGEDATA */
+	offsetof(RSHDR, rsh_object) / sizeof(UWORD),   /* R_OBSPEC */
+	offsetof(RSHDR, rsh_tedinfo) / sizeof(UWORD),  /* R_TEPTEXT */
+	offsetof(RSHDR, rsh_tedinfo) / sizeof(UWORD),  /* R_TEPTMPLT */
+	offsetof(RSHDR, rsh_tedinfo) / sizeof(UWORD),  /* R_TEPVALID */
+	offsetof(RSHDR, rsh_iconblk) / sizeof(UWORD),  /* R_IBPMASK */
+	offsetof(RSHDR, rsh_iconblk) / sizeof(UWORD),  /* R_IBPDATA */
+	offsetof(RSHDR, rsh_iconblk) / sizeof(UWORD),  /* R_IBPTEXT */
+	offsetof(RSHDR, rsh_bitblk) / sizeof(UWORD),   /* R_BIPDATA */
+	offsetof(RSHDR, rsh_frstr) / sizeof(UWORD),    /* R_FRSTR */
+	offsetof(RSHDR, rsh_frimg) / sizeof(UWORD)     /* R_FRIMG */
+};
+
+static unsigned char const rg_size_tab[] = {
+	0,                /* rsh_vrsn: unused */
+	sizeof(OBJECT),   /* rsh_object */
+	sizeof(TEDINFO),  /* rsh_tedinfo */
+	sizeof(ICONBLK),  /* rsh_iconblk */
+	sizeof(BITBLK),   /* rsh_bitblk */
+	sizeof(char *),   /* rsh_frstr */
+	0,                /* rsh_string: unused */
+	0,                /* rsh_imdata: unused */
+	sizeof(BITBLK *), /* rsh_frimg */
+	sizeof(OBJECT *)  /* rsh_trindex */
+};
+
+static unsigned char const rg_offset_tab[] = {
+	0,                            /* R_TREE */
+	0,                            /* R_OBJECT */
+	0,                            /* R_TEDINFO */
+	0,                            /* R_ICONBLK */
+	0,                            /* R_BITBLK */
+	0,                            /* R_STRING */
+	offsetof(BITBLK, bi_pdata),   /* R_IMAGEDATA */
+	offsetof(OBJECT, ob_spec),    /* R_OBSPEC */
+	offsetof(TEDINFO, te_ptext),  /* R_TEPTEXT */
+	offsetof(TEDINFO, te_ptmplt), /* R_TEPTMPLT */
+	offsetof(TEDINFO, te_pvalid), /* R_TEPVALID */
+	offsetof(ICONBLK, ib_pmask),  /* R_IBPMASK */
+	offsetof(ICONBLK, ib_pdata),  /* R_IBPDATA */
+	offsetof(ICONBLK, ib_ptext),  /* R_IBPTEXT */
+	offsetof(BITBLK, bi_pdata),   /* R_BIPDATA */
+	0,                            /* R_FRSTR */
+	0                             /* R_FRIMG */
+};
+
+
+static void *get_sub(RSHDR *rscHdr, WORD idx, ULONG offset, size_t size)
 {
-	gl_rsc = rsc;
-	gl_global = global;
-	*((RSHDR **)&global[7]) = rsc;
-	fix_treeindex();
-	fix_tedinfo();
-	fix_nptr(gl_rsc->rsh_nib, R_IBPMASK);
-	fix_nptr(gl_rsc->rsh_nib, R_IBPDATA);
-	fix_nptr(gl_rsc->rsh_nib, R_IBPTEXT);
-
-	fix_nptr(gl_rsc->rsh_nbb, R_BIPDATA);
-	fix_nptr(gl_rsc->rsh_nstring, R_FRSTR);
-	fix_nptr(gl_rsc->rsh_nimages, R_FRIMG);
-	fix_object();
-}
-
-
-static void *get_sub(WORD idx, ULONG offset, size_t size)
-{
-	char *base = ((char *)gl_rsc) + offset;
+	char *base = ((char *)rscHdr) + offset;
 	base += (LONG)idx * size;
 	return base;
 }
 
 
-static void fix_treeindex(void)
+#define get_sub(rscHdr, idx, offset, size) ((void *)(((UBYTE *)rscHdr) + (offset) + ((size) * (idx))))
+
+
+static void *get_address(RSHDR *rscHdr, UWORD rstype, UWORD rsindex)
 {
-	UWORD count;
-	OBJECT **RootTree;
-
-	count = gl_rsc->rsh_ntree;
-
-	RootTree = (OBJECT **)get_sub(R_TREE, gl_rsc->rsh_trindex, sizeof(OBJECT *));
-	*((OBJECT ***)&gl_global[5]) = RootTree;
-
-	while (count != 0)
-	{
-		count--;
-		fix_long((LONG *) (count * sizeof (OBJECT *) + (LONG) RootTree));
-	}
-}
-
-
-static void fix_tedinfo(void)
-{
-	UWORD count;
-	TEDINFO *ted;
+    unsigned char type;
+	void *the_addr;
 	
-	count = gl_rsc->rsh_nted;
-
-	while (count != 0)
-	{
-		count--;
-		ted = get_address(R_TEDINFO, count);
-		/* BUG: using strlen here is wrong */
-		if (fix_ptr(R_TEPTEXT, count))
-			ted->te_txtlen = (WORD)strlen(ted->te_ptext) + 1;
-		if (fix_ptr(R_TEPTMPLT, count))
-			ted->te_tmplen = (WORD)strlen(ted->te_ptmplt) + 1;
-		if (fix_ptr(R_TEPVALID, count))
-			;
-	}
-}
-
-
-static void fix_nptr(WORD idx, WORD ob_type)
-{
-	while (idx != 0)
-	{
-		idx--;
-		fix_long((LONG *)get_address(ob_type, idx));
-	}
-}
-
-
-static int fix_ptr(WORD type, WORD idx)
-{
-	WORD _idx = idx;
-	_idx = _idx;
-	return fix_long((LONG *)get_address(type, _idx));
-}
-
-
-static int fix_long(LONG *lptr)
-{
-	LONG base;
-
-	base = *lptr;
-	if (base == 0)
-		return FALSE;
-	base += (LONG)gl_rsc;
-	*lptr = base;
-	return TRUE;
-}
-
-
-static void *get_address(WORD type, WORD idx)
-{
-	void *the_addr = NULL;
-
-	switch (type)
+	if (rstype > R_FRIMG)
+		return NULL;
+	type = rg_type_tab[rstype];
+	the_addr = get_sub(rscHdr, rsindex, ((const UWORD *)rscHdr)[type] + rg_offset_tab[rstype], rg_size_tab[type]);
+	
+	/* addresses that must be dereferenced */
+	switch (rstype)
 	{
 	case R_TREE:
-		{
-			OBJECT **ptr = (OBJECT **)get_sub(0, gl_rsc->rsh_trindex, sizeof(OBJECT *));
-			the_addr = ptr[idx];
-		}
-		break;
-
-	case R_OBJECT:
-		the_addr = get_sub(idx, gl_rsc->rsh_object, sizeof (OBJECT));
-		break;
-
-	case R_TEDINFO:
-	case R_TEPTEXT:
-		the_addr = get_sub(idx, gl_rsc->rsh_tedinfo, sizeof (TEDINFO));
-		break;
-
-	case R_ICONBLK:
-	case R_IBPMASK:
-		the_addr = get_sub(idx, gl_rsc->rsh_iconblk, sizeof (ICONBLK));
-		break;
-
-	case R_BITBLK:
-	case R_BIPDATA:
-		the_addr = get_sub(idx, gl_rsc->rsh_bitblk, sizeof (BITBLK));
-		break;
-
-	case R_OBSPEC:
-		{
-			OBJECT *ptr = (OBJECT *)get_sub(idx, gl_rsc->rsh_object, sizeof(OBJECT));
-			
-			the_addr = &ptr->ob_spec;
-		}
-		break;
-
-	case R_TEPTMPLT:
-	case R_TEPVALID:
-		{
-			TEDINFO *tedinfo = get_sub(idx, gl_rsc->rsh_tedinfo, sizeof(TEDINFO));
-			if (type == R_TEPVALID)
-				the_addr = &tedinfo->te_pvalid;
-			else
-				the_addr = &tedinfo->te_ptmplt;
-		}
-		break;
-
-	case R_IBPDATA:
-	case R_IBPTEXT:
-		{
-			ICONBLK *iconblk = (ICONBLK *)get_sub(idx, gl_rsc->rsh_iconblk, sizeof (ICONBLK));
-			if (type == R_IBPDATA)
-				the_addr = &iconblk->ib_pdata;
-			else
-				the_addr = &iconblk->ib_ptext;
-		}
-		break;
-
-	case R_IMAGEDATA:
-		the_addr = get_sub(idx, gl_rsc->rsh_frimg, sizeof(_UBYTE *));
-		if (the_addr)
-			the_addr = *((void **)the_addr);
-		break;
-
-	case R_FRIMG:
-		the_addr = get_sub(idx, gl_rsc->rsh_frimg, sizeof (_UBYTE *));
-		break;
-
 	case R_STRING:
-		the_addr = get_sub(idx, gl_rsc->rsh_frstr, sizeof(_UBYTE *));
-		if (the_addr)
-			the_addr = *((_VOID **)the_addr);
-		break;
-
-	case R_FRSTR:
-		the_addr = get_sub(idx, gl_rsc->rsh_frstr, sizeof (_UBYTE *));
-		break;
-
-	default:
+	case R_IMAGEDATA:
+		the_addr = *((void **)the_addr);
 		break;
 	}
-
+	
 	return the_addr;
 }
 
 
-static void fix_object(void)
+static void fix_long(RSHDR *rscHdr, LONG *lptr)
 {
-	UWORD count;
-	OBJECT *obj;
+	LONG base;
 
-	count = gl_rsc->rsh_nobs;
-
-	while (count != 0)
+	base = *lptr;
+	if (base != 0)
 	{
-		WORD type;
-
-		count--;
-		obj = (OBJECT *)get_address(R_OBJECT, count);
-		fix_obj(obj, 0);
-		type = obj->ob_type & 0xFF;
-		if (type != G_BOX && type != G_IBOX && type != G_BOXCHAR)
-		{
-			fix_long((LONG *) &obj->ob_spec);
-		}
+		base += (LONG)rscHdr;
+		*lptr = base;
 	}
 }
 
 
-static void fix_obj(OBJECT *tree, WORD obj)
+static void fix_nptr(RSHDR *rscHdr, WORD idx, WORD ob_type)
 {
-	WORD *p;
-	WORD i;
-	WORD flag;
-	
-	flag = 0;
-	i = 0;
-	p = &tree[obj].ob_x;
-	while (i++ < 4)
+	while (idx != 0)
 	{
-		fix_chp(p++, flag);
-		flag = flag ? 0 : 1;
+		idx--;
+		fix_long(rscHdr, (LONG *)get_address(rscHdr, ob_type, idx));
 	}
+}
+
+
+static void fix_ptr(RSHDR *rscHdr, WORD type, WORD idx)
+{
+	fix_long(rscHdr, (LONG *)get_address(rscHdr, type, idx));
 }
 
 
@@ -263,4 +139,90 @@ static void fix_chp(WORD *pcoord, int hor)
 		ncoord += (*pcoord >> 8) & 0xff;
 	}
 	*pcoord = ncoord;
+}
+
+
+static void fix_obj(OBJECT *tree, WORD obj)
+{
+	WORD *p;
+	WORD i;
+	WORD flag;
+	
+	flag = 0;
+	i = 0;
+	p = &tree[obj].ob_x;
+	while (i++ < 4)
+	{
+		fix_chp(p++, flag);
+		flag = flag ? 0 : 1;
+	}
+}
+
+
+void _rsrc_rcfix(WORD *global, RSHDR *rscHdr)
+{
+	*((RSHDR **)&global[7]) = rscHdr;
+	
+	/* fix_treeindex(); */
+	{
+		UWORD count;
+		OBJECT **RootTree;
+	
+		count = rscHdr->rsh_ntree;
+	
+		RootTree = (OBJECT **)get_sub(rscHdr, 0, rscHdr->rsh_trindex, sizeof(OBJECT *));
+		*((OBJECT ***)&global[5]) = RootTree;
+	
+		while (count != 0)
+		{
+			count--;
+			fix_long(rscHdr, (LONG *) (count * sizeof (OBJECT *) + (LONG) RootTree));
+		}
+	}
+
+	/* fix_tedinfo(); */
+	{
+		UWORD count;
+		
+		count = rscHdr->rsh_nted;
+	
+		while (count != 0)
+		{
+			count--;
+			fix_ptr(rscHdr, R_TEPTEXT, count);
+			fix_ptr(rscHdr, R_TEPTMPLT, count);
+			fix_ptr(rscHdr, R_TEPVALID, count);
+		}
+	}
+
+
+	fix_nptr(rscHdr, rscHdr->rsh_nib, R_IBPMASK);
+	fix_nptr(rscHdr, rscHdr->rsh_nib, R_IBPDATA);
+	fix_nptr(rscHdr, rscHdr->rsh_nib, R_IBPTEXT);
+
+	fix_nptr(rscHdr, rscHdr->rsh_nbb, R_BIPDATA);
+	fix_nptr(rscHdr, rscHdr->rsh_nstring, R_FRSTR);
+	fix_nptr(rscHdr, rscHdr->rsh_nimages, R_FRIMG);
+
+	/* fix_object();  */
+	{
+		UWORD count;
+		OBJECT *obj;
+	
+		count = rscHdr->rsh_nobs;
+	
+		while (count != 0)
+		{
+			WORD type;
+	
+			count--;
+			obj = (OBJECT *)get_address(rscHdr, R_OBJECT, count);
+			fix_obj(obj, 0);
+			type = obj->ob_type & 0xFF;
+			if (type != G_BOX && type != G_IBOX && type != G_BOXCHAR)
+			{
+				fix_long(rscHdr, (LONG *) &obj->ob_spec);
+			}
+		}
+	}
 }
