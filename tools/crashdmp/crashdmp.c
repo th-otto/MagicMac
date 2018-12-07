4de87493f5e2dd46940b95412dd9195c304c40d1
Thorsten Otto <admin@tho-otto.de>
2018-12-07 09:24:55 +0100

    crashdmp: translate output to english

--- tools/crashdmp/crashdmp.c
+++ tools/crashdmp/crashdmp.c
@@ -3,7 +3,7 @@
 *                          CRASHDMP		14.7.87
 *                          ========
 *
-*					  letzte énderung:	3.12.88
+*					  letzte Aenderung:	3.12.88
 *
 *
 * Zweck:  Die Daten der letzten Exception (Bomben) dekodieren und
@@ -36,11 +36,11 @@
 #define EN_TRACE	9
 #define EN_LINEA	10
 #define EN_LINEF	11
-#define EN_COPROT	13	/* 68030 */
-#define EN_FORMAT	14	/* 68030 */
+#define EN_COPROT	13					/* 68030 */
+#define EN_FORMAT	14					/* 68030 */
 #define EN_TRAP0	32
 #define EN_TRAP15	47
-#define EN_MMUCNF	56	/* 68030 */
+#define EN_MMUCNF	56					/* 68030 */
 
 
 
@@ -50,216 +50,51 @@
 #define CRLF              "\r\n"
 
 
-char ausgabe[] = "  XX = 00000000";
+static char ausgabe[] = "  XX = 00000000";
 
-long get_cookie( long typ );
-void setint(char *adresse, unsigned int wert);
-void setlong(char *adresse, unsigned long wert);
-void print_sr(int  st);
-void print_pc(long pc);
-void print_op(int  op);
-void print_ad(long ad);
-void print_xt(int  xt);
-int  cpu = 0;
-
-void main()
-{
-              long usp;
-     register int  i;
-     register unsigned char c;
-              char *fehler;
-
-
-
-     usp = Super(0L);						/* Supervisormodus */
-     cpu = (int) get_cookie('_CPU');
-
-     if   (*proc_lives != PROC_MAGIC)
-     	{
-     	Super((int *) usp);					/* Usermodus */
-     	Pterm0();
-     	}
-
-	/* Fehlertyp ausgeben */
-	/* ------------------ */
-
-     c = *((unsigned char *) proc_pc);			/* Fehlernummer */
-     if	(c >= EN_TRAP0 && c <= EN_TRAP15)
-     	{
-     	fehler = "Trap #$0";
-     	fehler[7] = c - EN_TRAP0 + '0';
-     	if	(fehler[7] > '9')
-     		fehler[7] += 'a'-'9';
-     	}
-     else	{
-     	switch (c) {
-      		case  EN_BUS:		fehler = "Busfehler"   ; break;
-      		case  EN_ADDR: 	fehler = "Adressfehler"; break;
-	          case  EN_ILLEGAL:	fehler = "Illegal"     ; break;
-	          case  EN_DIV0: 	fehler = "Div. durch 0"; break;
-	          case  EN_CHK:		fehler = "CHK"         ; break;
-	          case  EN_TRAPV: 	fehler = "TRAPV"       ; break;
-	          case  EN_PRIV:		fehler = "Priv. Befehl"; break;
-	          case  EN_TRACE:	fehler = "TRACE"       ; break;
-	          case  EN_LINEA:	fehler = "Line - A"    ; break;
-	          case  EN_LINEF:	fehler = "Line - F"    ; break;
-			case  EN_COPROT:	fehler = "Koprozessor-Protokoll"; break;
-			case	 EN_FORMAT:	fehler = "Format";break;
-			case  EN_MMUCNF:	fehler = "MMU-Konfiguration"; break;
-	          default: 			fehler = "        ";
-	                    		setlong(fehler, c)	   ;	break;
-      		}
-      	}
-     Cconws("Fehler : ");
-     Cconws(fehler);
-     Cconws(CRLF);
-     Cconws(CRLF);
-
-	/* Register ausgeben */
-	/* ----------------- */
-
-	for	(i = 0; i < 8; i++)
-		{
-          ausgabe[3] = '0' + (char) i;
-          ausgabe[2] = 'd';
-          setlong(ausgabe+7, proc_regs[i]);
-          Cconws(ausgabe);
-          Cconws("                    ");
-          ausgabe[2] = 'a';
-          setlong(ausgabe+7, proc_regs[i+8]);
-          if	(i < 7)
-          	Cconws(ausgabe);
-          else	{
-          	Cconws(" ssp");
-          	Cconws(ausgabe+4);
-          	}
-          Cconws(CRLF);
-		}
-
-	/* USP ausgeben */
-	/* ------------ */
-
-     Cconws("                                    usp");
-     setlong(ausgabe+7, *proc_usp);
-     Cconws(ausgabe+4);
-     Cconws(CRLF);
-     Cconws(CRLF);
-
-	if	(c == EN_BUS || c == EN_ADDR)
-		{
-		if	(cpu > 0)
-			{
-			print_pc(* ((long *) (proc_stk+1)) );
-			print_sr(proc_stk[0]);
-			print_ad(* ((long *) (proc_stk+8)) );
-			print_xt(proc_stk[5]);
-			}
-		else	{
-			print_pc(* ((long *) (proc_stk+5)) );
-			print_sr(proc_stk[4]);
-			print_ad(* ((long *) (proc_stk+1)) );
-			print_op(proc_stk[3]);
-			print_xt(proc_stk[0]);
-			}
-		}
-	else	{
-		print_pc(* ((long *) (proc_stk+1)) );
-		print_sr(proc_stk[1]);
-		}
-
-	/* Stack ausgeben */
-	/* -------------- */
-
-	ausgabe[11] = '\0';
-	Cconws("\r\nStack:\r\n");
-	for	(i = 0; i < 16; i++)
-		{
-          ausgabe[3] = '0' + (char) i%10;
-          ausgabe[2] = '0' + (char) i/10;
-          ausgabe[1] = 's';
-          setint(ausgabe+7, proc_stk[i]);
-          Cconws(ausgabe);
-          Cconws((i % 2) ? CRLF : "   ");
-		}
-
-     Super((int *) usp);						/* Usermodus */
-     Pterm0();
-}
-
+static int cpu = 0;
 
 /**************************************************************
 *
 * Rechnet Wort in <len> - stellige Hex- Zahl um und gibt die
-* Anfangsadresse des Strings zurÅck.
+* Anfangsadresse des Strings zurueck.
 *
 **************************************************************/
 
-void setint(char *adresse, unsigned int wert)
+static void setint(char *adresse, unsigned int wert)
 {
-     register int  i,j;
+	register int i, j;
 
-
-	for	(i = 0; i < 4; i++)
-		{
-		j      = (int) (wert & 15);     /* 4 Bit fÅr Hex- Ziffer isolieren */
+	for (i = 0; i < 4; i++)
+	{
+		j = (int) (wert & 15);			/* 4 Bit fuer Hex- Ziffer isolieren */
 		wert >>= 4;
-		adresse[4-1-i] =  (j < 10) ? ('0'+(char) j) : ('a'+(char) (j-10));
-		}
+		adresse[4 - 1 - i] = (j < 10) ? ('0' + (char) j) : ('a' + (char) (j - 10));
+	}
 }
+
 
 
 /**************************************************************
 *
 * Rechnet Langwort in <len> - stellige Hex- Zahl um und gibt die
-* Anfangsadresse des Strings zurÅck.
+* Anfangsadresse des Strings zurueck.
 *
 **************************************************************/
 
-void setlong(char *adresse, unsigned long wert)
+static void setlong(char *adresse, unsigned long wert)
 {
-     register int  i,j;
+	register int i, j;
 
-
-	for	(i = 0; i < 8; i++)
-		{
-		j      = (int) (wert & 15);     /* 4 Bit fÅr Hex- Ziffer isolieren */
+	for (i = 0; i < 8; i++)
+	{
+		j = (int) (wert & 15);			/* 4 Bit fuer Hex- Ziffer isolieren */
 		wert >>= 4;
-		adresse[8-1-i] =  (j < 10) ? ('0'+(char) j) : ('a'+(char) (j-10));
-		}
+		adresse[8 - 1 - i] = (j < 10) ? ('0' + (char) j) : ('a' + (char) (j - 10));
+	}
 }
 
 
-/**************************************************************
-*
-* Gibt den Inhalt von <st> aus, als Prozessor - Status dekodiert
-*
-**************************************************************/
-
-void print_sr(int st)
-{
-	char *s;
-
-
-
-	Cconws("SR bei Auftreten des Fehlers: ");
-     if   (st < 0)
-          Cconws(" TR");
-     if   (st & (1 << 13))
-          Cconws(" SUP");
-     if   (st & (1 << 4))
-          Cconws(" EXT");
-     if   (st & (1 << 3))
-          Cconws(" NEG");
-     if   (st & (1 << 2))
-          Cconws(" ZER");
-     if   (st & (1 << 1))
-          Cconws(" OVF");
-     if   (st & 1)
-          Cconws(" CRY");
-     s = " INT =  \r\n";
-     s[7] = '0' + (char) ( (st >> 8) & 7);
-     Cconws(s);
-}
 
 
 /**************************************************************
@@ -268,12 +103,12 @@ void print_sr(int st)
 *
 **************************************************************/
 
-void print_pc(long pc)
+static void print_pc(long pc)
 {
-	Cconws("PC bei Auftreten des Fehlers:  ");
-     setlong(ausgabe+7, pc);
-     Cconws(ausgabe+7);
-     Cconws(CRLF);
+	Cconws("PC on exception:               ");
+	setlong(ausgabe + 7, pc);
+	Cconws(ausgabe + 7);
+	Cconws(CRLF);
 }
 
 
@@ -283,11 +118,11 @@ void print_pc(long pc)
 *
 **************************************************************/
 
-void print_op(int op)
+static void print_op(unsigned short op)
 {
-	Cconws("Fehlerauslîsender Opcode:      ");
-	setlong(ausgabe+7, (long) op);
-	Cconws(ausgabe+7+4);
+	Cconws("Opcode causing exception:      ");
+	setlong(ausgabe + 7, op);
+	Cconws(ausgabe + 7 + 4);
 	Cconws(CRLF);
 }
 
@@ -298,21 +133,53 @@ void print_op(int op)
 *
 **************************************************************/
 
-void print_xt(int xt)
+static void print_xt(int xt)
 {
-	static char *status[] = { "unbestimmt",
-						 "Usermodus Datenzugriff",
-						 "Usermodus Programm",
-						 "unbestimmt",
-						 "unbestimmt",
-						 "Supervisor Datenzugriff",
-						 "Supervisor Programm",
-						 "InterruptbestÑtigung" };
+	static const char *const status[] = {
+		"indetermined",
+		"Usermode data",
+		"Usermode program",
+		"indetermined",
+		"indetermined",
+		"Supervisor data",
+		"Supervisor program",
+		"Interrupt acknowledge"
+	};
 
-
-	Cconws("Prozessorstatus:               ");
+	Cconws("Prozessor state:               ");
 	Cconws(status[xt & 7]);
-	Cconws((xt & 16) ? ", Lesen \r\n" : ", Schreiben\r\n");
+	Cconws(xt & 16 ? ", Read \r\n" : ", Write\r\n");
+}
+
+
+/**************************************************************
+*
+* Gibt den Inhalt von <st> aus, als Prozessor - Status dekodiert
+*
+**************************************************************/
+
+static void print_sr(int st)
+{
+	char *s;
+
+	Cconws("SR on exception:              ");
+	if (st < 0)
+		Cconws(" TR");
+	if (st & (1 << 13))
+		Cconws(" SUP");
+	if (st & (1 << 4))
+		Cconws(" EXT");
+	if (st & (1 << 3))
+		Cconws(" NEG");
+	if (st & (1 << 2))
+		Cconws(" ZER");
+	if (st & (1 << 1))
+		Cconws(" OVF");
+	if (st & 1)
+		Cconws(" CRY");
+	s = " INT =  \r\n";
+	s[7] = '0' + (char) ((st >> 8) & 7);
+	Cconws(s);
 }
 
 
@@ -322,12 +189,12 @@ void print_xt(int xt)
 *
 **************************************************************/
 
-void print_ad(long ad)
+static void print_ad(long ad)
 {
-	Cconws("Fehler bei Zugriff auf:        ");
-     setlong(ausgabe+7, ad);
-     Cconws(ausgabe+7);
-     Cconws(CRLF);
+	Cconws("Error while accessing address: ");
+	setlong(ausgabe + 7, ad);
+	Cconws(ausgabe + 7);
+	Cconws(CRLF);
 }
 
 /*************************************************************
@@ -336,19 +203,173 @@ void print_ad(long ad)
 *
 *************************************************************/
 
-long get_cookie( long typ )
+static long get_cookie(long typ)
 {
-	register unsigned long *p_cookies;
+	unsigned long *p_cookies;
 
-	p_cookies = (unsigned long *) (* P_COOKIES);
-	while(p_cookies && *p_cookies)
+	p_cookies = (unsigned long *) (*P_COOKIES);
+	while (p_cookies && *p_cookies)
+	{
+		if (*p_cookies == typ)
 		{
-		if	(*p_cookies == typ)
-			{
 			p_cookies++;
-			return(*p_cookies);
-			}
-		p_cookies += 2;
+			return (*p_cookies);
 		}
-	return(0L);
+		p_cookies += 2;
+	}
+	return 0;
+}
+
+
+int main(void)
+{
+	long usp;
+	register int i;
+	register unsigned char c;
+	char *fehler;
+
+	usp = Super(0L);					/* Supervisormodus */
+	cpu = (int) get_cookie('_CPU');
+
+	if (*proc_lives != PROC_MAGIC)
+	{
+		Super((int *) usp);				/* Usermodus */
+		return 0;
+	}
+
+	/* Fehlertyp ausgeben */
+	/* ------------------ */
+
+	c = *((unsigned char *) proc_pc);	/* Fehlernummer */
+	if (c >= EN_TRAP0 && c <= EN_TRAP15)
+	{
+		fehler = "Trap #$0";
+		fehler[7] = c - EN_TRAP0 + '0';
+		if (fehler[7] > '9')
+			fehler[7] += 'a' - '9';
+	} else
+	{
+		switch (c)
+		{
+		case EN_BUS:
+			fehler = "bus error";
+			break;
+		case EN_ADDR:
+			fehler = "address error";
+			break;
+		case EN_ILLEGAL:
+			fehler = "illegal";
+			break;
+		case EN_DIV0:
+			fehler = "div. by 0";
+			break;
+		case EN_CHK:
+			fehler = "CHK";
+			break;
+		case EN_TRAPV:
+			fehler = "TRAPV";
+			break;
+		case EN_PRIV:
+			fehler = "privilege violation";
+			break;
+		case EN_TRACE:
+			fehler = "TRACE";
+			break;
+		case EN_LINEA:
+			fehler = "Line - A";
+			break;
+		case EN_LINEF:
+			fehler = "Line - F";
+			break;
+		case EN_COPROT:
+			fehler = "Coprozessor protocol";
+			break;
+		case EN_FORMAT:
+			fehler = "Format";
+			break;
+		case EN_MMUCNF:
+			fehler = "MMU configuration";
+			break;
+		default:
+			fehler = "        ";
+			setlong(fehler, c);
+			break;
+		}
+	}
+	Cconws("Error : ");
+	Cconws(fehler);
+	Cconws(CRLF);
+	Cconws(CRLF);
+
+	/* Register ausgeben */
+	/* ----------------- */
+
+	for (i = 0; i < 8; i++)
+	{
+		ausgabe[3] = '0' + (char) i;
+		ausgabe[2] = 'd';
+		setlong(ausgabe + 7, proc_regs[i]);
+		Cconws(ausgabe);
+		Cconws("                    ");
+		ausgabe[2] = 'a';
+		setlong(ausgabe + 7, proc_regs[i + 8]);
+		if (i < 7)
+		{
+			Cconws(ausgabe);
+		} else
+		{
+			Cconws(" ssp");
+			Cconws(ausgabe + 4);
+		}
+		Cconws(CRLF);
+	}
+
+	/* USP ausgeben */
+	/* ------------ */
+
+	Cconws("                                    usp");
+	setlong(ausgabe + 7, *proc_usp);
+	Cconws(ausgabe + 4);
+	Cconws(CRLF);
+	Cconws(CRLF);
+
+	if (c == EN_BUS || c == EN_ADDR)
+	{
+		if (cpu > 0)
+		{
+			print_pc(*((long *) (proc_stk + 1)));
+			print_sr(proc_stk[0]);
+			print_ad(*((long *) (proc_stk + 8)));
+			print_xt(proc_stk[5]);
+		} else
+		{
+			print_pc(*((long *) (proc_stk + 5)));
+			print_sr(proc_stk[4]);
+			print_ad(*((long *) (proc_stk + 1)));
+			print_op(proc_stk[3]);
+			print_xt(proc_stk[0]);
+		}
+	} else
+	{
+		print_pc(*((long *) (proc_stk + 1)));
+		print_sr(proc_stk[1]);
+	}
+
+	/* Stack ausgeben */
+	/* -------------- */
+
+	ausgabe[11] = '\0';
+	Cconws("\r\nStack:\r\n");
+	for (i = 0; i < 16; i++)
+	{
+		ausgabe[3] = '0' + (char) i % 10;
+		ausgabe[2] = '0' + (char) i / 10;
+		ausgabe[1] = 's';
+		setint(ausgabe + 7, proc_stk[i]);
+		Cconws(ausgabe);
+		Cconws((i % 2) ? CRLF : "   ");
+	}
+
+	Super((int *) usp);					/* Usermodus */
+	return 0;
 }
