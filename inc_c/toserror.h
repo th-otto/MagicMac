5cd247b92840dd482422034e62d66393b7d9a1e8
Thorsten Otto <admin@tho-otto.de>
2018-12-21 12:22:38 +0100

    Add some MiNT error codes

--- inc_c/toserror.h
+++ inc_c/toserror.h
@@ -30,30 +30,79 @@
 #define ENSMEM	-39L	/* insufficient memory			 8 */
 #define EIMBA	-40L	/* invalid memory block address 	 9 */
 #define EDRIVE	-46L	/* invalid drive was specified	15 */
-#define ENSAME -48L /* MV between two different drives 17 */
+#define ENSAME - 48L	/* MV between two different drives 17 */
 #define ENMFIL	-49L	/* no more files				18 */
+
 
 /* our own inventions */
 
-#define ERANGE	-64L	/* range error					33 */
-#define EINTRN	-65L	/* internal error				34 */
-#define EPLFMT	-66L	/* invalid program load format	35 */
-#define EGSBF	-67L	/* setblock failure 			36 */
+#define ERANGE	-64		/* range error (EBADARG in MiNT) */
+#define EINTRN	-65 	/* internal error				34 */
+#define EPLFMT	-66 	/* invalid program load format	35 */
+#define EGSBF	-67 	/* setblock failure 			36 */
 
 /* KAOS extensions */
 
-#define EBREAK	-68L	/* user break (^C)				37 */
-#define EXCPT	-69L	/* 68000- exception ("bombs")		38 */
+#define EBREAK	-68 	/* user break (^C)				37 */
+#define EXCPT	-69 	/* 68000- exception ("bombs")		38 */
 
 /* MiNT extensions */
 
-#define ELOCKED -58L
-#define ENSLOCK -59L
+#define EROFS     EWRPRO
+#define ENODEV    EUNDEV
+#define ENOMEDIUM EOTHER
+#define ESRCH   -20     /* no such process */
+#define ECHILD  -21     /* no child processes */
+#define EDEADLK -22     /* Resource deadlock would occur */
+#define ENOTBLK -23     /* Block device required */
+#define EISDIR  -24     /* Is a directory */
+#define EINVAL  -25     /* Invalid argument */
+#define EFTYPE  -26     /* Inappropriate file type or format */
+#define EILSEQ  -27     /* Illegal byte sequence */
+#define ENOSYS  EINVFN
+#define ENOENT  EFILNF
+#define ENOTDIR EPTHNF
+#define EMFILE  ENHNDL
+#define EACCES  EACCDN
+#define EBADF   EIHNDL
+#define EPERM   -38     /* Operation not permitted */
+#define ENOMEM  ENSMEM
+#define EFAULT  EIMBA
+#define ENXIO   EDRIVE
+#define EXDEV   ENSAME
+#define ENMFILES ENMFIL
+#define ENFILE  -50     /* File table overflow */
+#define ELOCKED -58     /* Locking conflict */
+#define ENSLOCK -59     /* No such lock */
+#define EBADARG -64		/* Bad argument */
+#define EINTERNAL EINTRN
+#define ENOEXEC  EPLFMT
+#define ETXTBSY -70		/* Text file busy */
+#define EFBIG   -71		/* File too big */
+#define ELOOP   -80		/* too many symlinks in path */
+#define EPIPE   -81		/* Broken pipe */
+#define EMLINK  -82		/* Too many links */
+#define ENOTEMPTY -83	/* Directory not empty */
+#define EEXIST   -85	/* File exists */
+#define ENAMETOOLONG -86 /* Name too long */
+#define ENOTTY   -87	/* Not a tty */
+#define _ERANGE  -88	/* Range error; conflicts in MagiC */
+#define EDOM     -89	/* Domain error */
+#define EIO      -90	/* I/O error */
+#define ENOSPC   -91	/* No space left on device */
+
+#define EINTR    -128	/* Interrupted function call */
+
+/* Falcon XBIOS errors.  */
+#define ESNDLOCKED -129	/* Sound system is already locked */
+#define ESNDNOTLOCK -128 /* Sound system is not locked */
+/* note1: sometimes incorrectly defined as -130 */
+/* note2: conflicts with EINTR from MiNT */
+
 
 /*  MagiC extensions */
 
-#define EBREAK -68L /* user break (^C)                         KAOS 1.2 */
-#define EXCPT  -69L /* 68000- exception ("bombs")              KAOS 1.2 */
-#define EPTHOV -70L /* path overflow                           MAG!X    */
-#define ELOOP  -80L /* too many symlinks in path               MiNT */
+#define EBREAK -68  /* user break/Aborted by user              KAOS 1.2 */
+#define EXCPT  -69  /* 68000- exception ("bombs")              KAOS 1.2 */
+#define EPTHOV -70  /* path overflow (conflicts with ETXTBSY)  MAG!X    */
 
