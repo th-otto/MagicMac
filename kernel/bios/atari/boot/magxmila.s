b952edd13c1d5e3e0eb06ecb7bc7140b25bac090
Thorsten Otto <admin@tho-otto.de>
2019-11-16 12:08:42 +0100

    Replace some magic constants

--- kernel/bios/atari/boot/magxmila.s
+++ kernel/bios/atari/boot/magxmila.s
@@ -11,11 +11,13 @@
 fstrm_beg EQU  ____md
 os_chksum EQU  trp14ret
 
+sizeof_PH equ 28
+
         .TEXT
 
         SUPER
 
-        movea.l $0004(a7),a1                    ; BasePagePointer from Stack
+        movea.l 4(a7),a1                        ; BasePagePointer from Stack
         lea     stack(pc),a7
         movea.w #$0100,a0                       ; Programmlaenge + $100
         adda.l  p_tlen(a1),a0
@@ -25,7 +27,7 @@ os_chksum EQU  trp14ret
         move.l  a1,-(a7)
         clr.w   -(a7)
         gemdos   Mshrink
-        adda.w  #$000C,a7
+        adda.w  #12,a7
         tst.l   d0
         bmi     err
 
@@ -71,7 +73,7 @@ is_milan:
         move.w  #-1,-(a7)
         bios    Kbshift
         addq.l  #4,a7
-        andi.b  #$03,d0
+        andi.b  #3,d0
         subq.b  #3,d0                           ; Shift preset?
         bne.s   do_install
         lea     dont_install(pc),a0
@@ -118,7 +120,7 @@ do_install:
         move.l  d6,-(a7)                        ; count
         move.w  d7,-(a7)                        ; handle
         gemdos  Fread
-        adda.w  #$000C,a7
+        adda.w  #12,a7
         move.l  d0,-(a7)
         move.w  d7,-(a7)
         gemdos  Fclose
@@ -132,8 +134,8 @@ do_install:
 
 * pruefen, ob MagiX, und a5 auf Ladeadresse
 
-        movea.l $0030(a6),a0
-        lea     $1C(a6,a0.l),a0
+        movea.l sizeof_PH+os_magic(a6),a0
+        lea     sizeof_PH(a6,a0.l),a0
         cmpi.l  #$87654321,(a0)+
         bne     err
         movea.l (a0)+,a5
@@ -148,8 +150,8 @@ do_install:
         addq.l  #2,a7
 ;buf+1c ist das Programm ohne den Programmheader
 ;bei Offset 0x14 steht der Zeiger auf die AES-Variablen
-        movea.l $0030(a6),a0
-        lea     $1C(a6,a0.l),a0
+        movea.l (os_magic+sizeof_PH)(a6),a0
+        lea     sizeof_PH(a6,a0.l),a0
         lea     $7A(a0),a0
         cmpi.l  #'____',(a0)                    ; Sicherheitsfrage fuer alten Kernel
         bne.s   do_reloc
@@ -164,16 +166,16 @@ namecpy_loop:
 * MagiX relozieren
 
 do_reloc:
-        move.l  $0002(a6),d0                    ; Laenge von TEXT
-        add.l   $0006(a6),d0                    ; Laenge von DATA
+        move.l  2(a6),d0                        ; Laenge von TEXT
+        add.l   6(a6),d0                        ; Laenge von DATA
         move.l  d0,d5                           ; tatsaechliche Laenge von MagiX
-        add.l   $000E(a6),d0                    ; Laenge von SYM
+        add.l   14(a6),d0                       ; Laenge von SYM
 
-        lea     $1C(a6,d0.l),a3                 ; Beginn der Relocation- Daten
-        lea     $00(a6,d6.l),a2                 ; Dateiende
+        lea     sizeof_PH(a6,d0.l),a3           ; Beginn der Relocation- Daten
+        lea     0(a6,d6.l),a2                   ; Dateiende
         cmpa.l  a2,a3
         bcc.s   end_reloc
-        lea     $001C(a6),a0
+        lea     sizeof_PH(a6),a0
         move.l  (a3)+,d0                        ; erstes Relocation- Langwort
 reloc_loop:
         adda.l  d0,a0
@@ -182,19 +184,19 @@ reloc_loop:
 reloc_loop2:
         cmpa.l  a2,a3
         bhi.s   end_reloc                       ; Dateiende!
-        moveq   #$00,d0
+        moveq   #0,d0
         move.b  (a3)+,d0
         beq.s   end_reloc                       ; Ende der Tabelle
-        cmpi.b  #$01,d0
+        cmpi.b  #1,d0
         bne.s   reloc_loop
-        lea     $00FE(a0),a0
+        lea     254(a0),a0
         bra.s   reloc_loop2
 
 end_reloc:
 * Checksumme ermitteln
-        lea     $001C(a6),a0
-        lea     $00(a0,d5.l),a1
-        moveq   #$00,d0
+        lea     sizeof_PH(a6),a0
+        lea     0(a0,d5.l),a1
+        moveq   #0,d0
 os_chkloop:
         add.l   (a0)+,d0
         cmpa.l  a0,a1
@@ -213,8 +215,8 @@ os_chkloop:
         addq.l  #6,a7
         ori     #$0700,sr
         lea     toscopy(pc),a1                  ; aktuelle Adresse der Startroutine
-        lea     $0600  ,a0                      ; neue Adresse der Startroutine
-        move.w  #$0016,d0
+        lea     $0600,a0                        ; neue Adresse der Startroutine
+        move.w  #((toscopyend-toscopy+3)/4)-1,d0
 startcopy:
         move.l  (a1)+,(a0)+
         dbf     d0,startcopy
@@ -222,7 +224,7 @@ startcopy:
 
 toscopy:
         movea.l a5,a0
-        lea     $001C(a6),a1
+        lea     sizeof_PH(a6),a1
         move.l  d5,d0                           ; Laenge
         lsr.l   #3,d0                           ; /8
         cmpa.l  a0,a1
@@ -248,14 +250,16 @@ cpy_end:
         bra.s   startit
 
 cpy_st:
-        add.l   d5,$000C(a5)
-        movea.l $0014(a5),a0
+        add.l   d5,os_membot(a5)
+        movea.l os_membot(a5),a0
         cmpi.l  #$87654321,(a0)+
         bne.s   startit
         add.l   d5,(a0)
 startit:
         clr.l   _hz_200
         jmp     (a5)
+toscopyend:
+
 
 NeuesMilanROMnoetig:
         lea     Text_Neue_Version_noetig(pc),a0
@@ -283,7 +287,7 @@ cconws_country:
         bsr.s   _chk_nat
         bne.s   cconws_country
         move.l  a0,-(a7)
-        move.w  #$0009,-(a7)                    ; Cconws
+        move.w  #9,-(a7)                        ; Cconws
         trap    #1
         addq.l  #6,a7
         rts
@@ -299,29 +303,28 @@ _chk_nxt:
 _chk_nxtstr:
         tst.b   (a0)+                           ; Zeichenkette ueberspringen
         bne.s   _chk_nxtstr
-        moveq   #$01,d0                         ; nicht gefunden
+        moveq   #1,d0                           ; nicht gefunden
         rts
 
 _chk_found:
         tst.b   (a0)+
         bge.s   _chk_found
 _chk_ende:
-        moveq   #$00,d0                         ; gefunden, a0 ausdrucken
+        moveq   #0,d0                           ; gefunden, a0 ausdrucken
         rts
 
 MilanTOS_einsprung:
-        dc.l    $00000000
+        dc.l    0
 machine:
-        dc.l    $00000000
+        dc.l    0
 titel:
         DC.B     -1
         DC.B     CR,LF,'MagiCMilan- BOOTER, ',$bd,' 1990-99 Andreas Kromke',CR,LF,0
 
 Text_Neue_Version_noetig:
-        dc.b    $01,$08,$FF,'Neuere Version des Milan-ROMs besorgen!',$0D,$0A
-        dc.b    $00,$02,$FF,'Le Milan ROM est tout vieux!',$0D
-        dc.b    $0A,$00,$FF,'Get newer version of Milan ROM!',$0D,$0A
-        dc.b    $00
+        dc.b    COUNTRY_DE,COUNTRY_SG,-1,'Neuere Version des Milan-ROMs besorgen!',CR,LF,0
+        dc.b    COUNTRY_FR,COUNTRY_SF,-1,'Le Milan ROM est tout vieux!',CR,LF,0
+        dc.b    -1,'Get newer version of Milan ROM!',CR,LF,0
 
 dont_install:
         DC.B    COUNTRY_DE,COUNTRY_SG,-1
